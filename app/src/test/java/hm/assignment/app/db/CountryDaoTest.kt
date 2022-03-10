d35193aea76096aab690737e6452fa869a869975
Kaggee <oskar.gustavsson@gmail.com>
2022-03-10 20:55:59 +0100

    #6 - Playing around with room tests, but keep getting mainlooper exceptions.

--- app/src/test/java/hm/assignment/app/db/CountryDaoTest.kt
+++ app/src/test/java/hm/assignment/app/db/CountryDaoTest.kt
@@ -6,25 +6,26 @@ import androidx.room.Room
 import hm.assignment.app.api.models.CountryModel
 import hm.assignment.app.dao.CountryLayerDatabase
 import hm.assignment.app.util.Constants
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.*
 import kotlinx.coroutines.test.*
-import kotlinx.coroutines.withContext
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 
 /**
  * Created on 2022-03-08.
  * CopyrightⒸ Kagge
  */
 
+@DelicateCoroutinesApi
 @ExperimentalCoroutinesApi
 class CountryDaoTest {
     private val context: Context = Mockito.mock(Context::class.java)
+    private val mainThreadSurrogate = newSingleThreadContext("UIThread")
     private val testDispatcher = StandardTestDispatcher()
 
     private lateinit var database: CountryLayerDatabase
@@ -34,29 +35,32 @@ class CountryDaoTest {
 
     @Before
     fun setupDatabase() {
-        Dispatchers.setMain(testDispatcher)
+        Dispatchers.setMain(mainThreadSurrogate)
         database = Room
             .inMemoryDatabaseBuilder(context, CountryLayerDatabase::class.java)
             .addMigrations(CountryLayerDatabase.MIGRATION_1_2)
             .build()
     }
 
-    @After
-    fun closeDatabase() {
-        database.close()
-        Dispatchers.resetMain()
-    }
-
     @Test
-    fun insert_Test() = runTest(testDispatcher) {
+    fun insert_Test() = runBlocking {
         val countries = arrayListOf<CountryModel>()
         countries.add(CountryModel("Western Sahara", "El Aaiún", "Africa", "EH"))
         countries.add(CountryModel("Jamaica", "Kingston", "Americas", "JM"))
         countries.add(CountryModel("Egypt", "Cairo", "Africa", "EG"))
 
+        var country: CountryModel? = null
+        database.countryDao().insertCountries(countries)
+        country = database.countryDao().getCountry(countries[1].name)
 
-            database.countryDao().insertCountries(countries)
-            val getTest = database.countryDao().getCountry(countries[1].name)
-            assertEquals(countries[1], getTest)
+        assertNotNull(country)
+        assertEquals(countries[1], country)
+    }
+
+    @After
+    fun tearDown() {
+        database.close()
+        Dispatchers.resetMain()
+        mainThreadSurrogate.close()
     }
 }
